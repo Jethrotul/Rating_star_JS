c279f6c6833eb812af0c0267f32a540efdb8e6f3
Diego Fernandez <jethrotul@gmail.com>
2019-07-23 11:37:48 +0200

    Cambios mayores

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
-/*class starRating extends HTMLElement {
-    constructor(){
-        super();
-
-        this.stars = [];
-
-        for (let i=0; i<5; i++) {
-            let s = document.createElement ('i');
-            s.className = 'fas';
-            this.appendChild(s);
-            this.stars.push(s);
-        }
+var vacio = 0;
+function relleno(e) {
+  if (vacio == 1) {
+    for (var i = 1; i < 6; i++) {
+      document.getElementById("estrella" + i).classList.remove("fas");
+      document.getElementById("estrella" + i).classList.add("far");
+      document.getElementById("estrella" + i).style.color = "#FD0";
+      vacio = 0;
     }
-}*/
-var registro = document.customElements.define('x-ratingestrellas', pol); 
+  }
+  for (var i = 1; i <= e.substr(8, 8); i++) {
+    document.getElementById("estrella" + i).classList.remove("far");
+    document.getElementById("estrella" + i).classList.add("fas");
+    document.getElementById(e).style.color = "#FD0";
+    vacio = 0;
+
+  }
+}
+function vaciado(e) {
+  if (vacio == 0) {
+    for (var i = 1; i <= e.substr(8, 8); i++) {
+      document.getElementById("estrella" + i).classList.remove("fas");
+      document.getElementById("estrella" + i).classList.add("far");
+    }
+  }
+}
+function fijado(e) {
+  for (var i = 1; i <= e.substr(8, 8); i++) {
+    document.getElementById("estrella" + i).classList.remove("far");
+    document.getElementById("estrella" + i).classList.add("fas");
+    document.getElementById("estrella" + i).style.color = "#F95";
+    vacio = 1;
+  }
+}
